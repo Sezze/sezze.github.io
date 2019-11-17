28f4341ca60316460ea25b4607396e2c1dcee009
Sezze <sebbe.aarnio@hotmail.com>
2019-11-17 19:43:15 +0200

    updated layout and new experiments

--- _nuxt/eb1a69ad88ce902c07f7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{276:function(n,t,e){"use strict";e.r(t);var c=e(10),component=Object(c.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("b-container",[this._v("\n  "+this._s(this.process)+"\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
